d22832802f212834e12434c7023600caca6e82be
christianchinsee-source <christianchinsee@gmail.com>
2026-06-02 18:49:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+}
+
+// Scroll animation
+const animatedItems = document.querySelectorAll(".slide-up");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+animatedItems.forEach((item) => {
+    observer.observe(item);
+});
